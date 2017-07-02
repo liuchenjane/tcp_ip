6cc5f88dcdc30c59bb7436c63de287d18c88ef53
liuchenjane <2386418764@qq.com>
2017-07-02 22:07:02 +0800

    Add files via upload

---
+++ tcp_ip6.2/README.md
@@ -0,0 +1,11 @@
+# tcp_ip
+循序渐进学习TCP网络编程
+
+## 1. 基本的TCP/IP服务器端和客户端<br>
+<br>
+服务器端一直监听本地的6666号端口，如果收到连接请求，将接收请求并接收客户端发来的消息；<br>
+客户端与服务器建立连接并发送一条消息。<br>
+<br>
+
+[TCP server实现](https://github.com/liuchenjane/tcp_ip/blob/master/server.cpp) <br>
+[TCP client实现](https://github.com/liuchenjane/tcp_ip/blob/master/client.cpp) <br>
---
+++ tcp_ip6.2/server.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+#define MAXLINE 4096
+
+int main(int argc, char ** argv){
+<<<<<<< HEAD
+    int listenfd,acceptfd;
+=======
+	int listenfd,connfd;
+>>>>>>> 2e8c54fa387a5a1f1a9d27beadb703700ba5540d
+	struct sockaddr_in servaddr;
+	char buff[4096];
+	int n;
+
+	if( (listenfd=socket(AF_INET,SOCK_STREAM,0))==-1 ){
+		printf("create socket error: %s(errno: %d)\n",strerror(errno),errno);
+		return 0;
+	}
+
+	memset(&servaddr,0,sizeof(servaddr));
+	servaddr.sin_family=AF_INET;
+	servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+	servaddr.sin_port=htons(6666);
+
+	if( bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr))==-1 ){
+		printf("bind socket error: %s(errno: %d)\n",strerror(errno),errno);
+		return 0;
+	}
+
+	if( listen(listenfd, 10)==-1){
+		printf("listen socket error:%s(errno: %d)\n",strerror(errno),errno);
+		return 0;
+	}
+
+	printf("=================waiting for client's request=================\n");
+<<<<<<< HEAD
+    if((acceptfd=accept(listenfd,(struct sockaddr*)NULL,NULL))==-1){
+        printf("accept socket error: %s(errno: %d)",strerror(errno),errno);
+    }
+    char recvMsg[100];
+    ssize_t readLen=read(acceptfd,recvMsg,sizeof(recvMsg));
+    if(readLen<0){
+        printf("read error: %s(errno: %d)\n",strerror(errno),errno);
+        return -1;
+    }
+    recvMsg[9]='\0';
+    printf("readLen:%d, recvMsg:%s\n",readLen,recvMsg);
+    sleep(5);
+    recvMsg[1]='9';
+    ssize_t writeLen=write(acceptfd, recvMsg, sizeof(recvMsg));
+    printf("writeLen: %d, sendMsg: %s\n",writeLen,recvMsg);
+    if(writeLen<0){
+        printf("writeLen error: %s(errno: %d)\n", strerror(errno),errno);
+        return -1;
+    }
+    close(acceptfd);
+=======
+	while(1){
+		if( (connfd=accept(listenfd, (struct sockaddr*)NULL,NULL))==-1 ){
+			printf("accept socket error: %s(errno: %d)\n",strerror(errno),errno);
+			continue;
+		}
+		n=recv(connfd,buff,MAXLINE,0);
+		buff[n]='\0';
+		printf("recv msg from client: %s\n",buff);
+		close(connfd);
+	}
+	close(listenfd);
+>>>>>>> 2e8c54fa387a5a1f1a9d27beadb703700ba5540d
+	return 0;
+}
